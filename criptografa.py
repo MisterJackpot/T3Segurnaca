79e4a1b12af3fd66ddfd15be874b326d4ba859a6
Gabriel Löff Sá de Moraes <gabriel.moraes@e-unicred.com.br>
2021-06-08 23:18:59 -0300

    Removendo partes desnecessarias

--- criptografa.py
+++
@@ -1,9 +0,0 @@
-from Crypto.Cipher import AES
-
-obj = AES.new('c2b1b713777115fc', AES.MODE_CBC, 'IV45IV45IV45IV45')
-message = "a1a2a3a4a5a6a7a81"
-ciphertext = obj.encrypt(message)
-
-print('IV45IV45IV45IV45'.encode('utf-8').hex())
-print(ciphertext.hex())
-
